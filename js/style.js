3d1240ac10e3c6a616863efb9d5dc98f6f3bde80
zarin-raim <zarinraim@gmail.com>
2021-11-06 18:53:08 +0100

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mobile-links").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mobile-links").style.width = "0";
+}
